667187a86e3c1c7da09661f7c00182d142bdbc12
Shenyang HUANG <shuang43@CS.McGill.CA>
2018-11-14 12:31:57 -0500

    Comments updated

--- runMLP_SAS.py
+++ runMLP_SAS.py
@@ -6,10 +6,11 @@ import os.path
 import pickle
 import datetime
 
-def setup_Fashion(ratio=0.1):
-    datasets = mlp_datasets.Incremental_MNISTlike("Fashion", 0.1, normalization="numerical")
-    return datasets
 
+'''
+randomize the order of which class arrives
+in the paper, a default order from 0-9 is used 
+'''
 def random_order(default=False):
     arr = np.arange(10)
     if(not default):
@@ -48,18 +49,27 @@ def main():
 
 
     '''
-    Training initial teacher network 
+    set up training, validation and testing data
     '''
     print("training step 0")
     datasets = mlp_datasets.Incremental_MNISTlike("MNIST", 0.1, order,normalization="numerical")
     datasets = mlp_datasets.sum_data(datasets,1,0)
 
+    '''
+    Training initial teacher network 
+    '''
     accuracy = main.train_Teacher(datasets[0], architecture=[16],
                                     input_dim=784, output_dim=2, 
                                     lr=lr,epochs=epochs, batch_size=32, 
                                     random_seed=seed,mute=True,test=True,class_curve=True)
     print("Teacher network achieved test accuracy " + str(accuracy[1]))
     params = main.export()
+
+    '''
+    SAS_Controller manages neural architecture internally
+    details see MLP_Controller/Controller.py
+    SAS stands for simple architecture search (it is the neural architecture search principle used here)
+    '''
     SAS_Controller = Controller.Controller(seed,'sas')
     SAS_Controller.param_import(params)
 
@@ -70,9 +80,16 @@ def main():
         print ("--------------------------------------------------------------")
         print("training step " + str(i-1))
         print ("saturate with new data")
+
+        '''
+        concatenate new data
+        '''
         datasets = mlp_datasets.sum_data(datasets,i,0)
         SASpath = "./logs/"+str(i)+"_1SAS"
 
+        '''
+        Train with concatenated dataset using early stopping
+        '''
         SAS_accuracy = SAS_Controller.execute(datasets[0],input_dim=784,
                                             output_dim=(i+1),lr=lr,epochs=epochs,
                                             batch_size=32,random_seed=seed,mute=True,
@@ -81,11 +98,19 @@ def main():
         print ("before SAS search, model achieved test accuracy of " + str(SAS_accuracy[1]))
         SAS_Controller.class_accuracy(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
 
+        '''
+        Retrain a baseline from scratch using the same architecture
+        '''
         base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,epochs=epochs,batch_size=32,random_seed=seed,mute=True,
                                                             test=True,noVal=False,upload=False,class_curve=True)
         print("Baseline achieved test accuracy " + str(base_accuracy[1]))
         SAS_params = SAS_Controller.export()
         
+        '''
+        Simple Architecture Search using 6 predefined guide lines 
+        For details see MLP_archSearch/MLP_SAS.py
+        obtained the best architecture for this step 
+        '''
         SAS_best = MLP_SAS.simple_search(datasets[0],SAS_params,
                                         SAS_accuracy[1],N_workers,
                                         restart_num=restart_num,output_dim=(i+1),
@@ -95,6 +120,10 @@ def main():
         print ("--------------------------------------------------------------")
         print("Doing Further Training")
 
+        '''
+        ensure that the training is complete for the current dataset 
+        usually runs very few epoches
+        '''
         SAS_Controller.param_import(SAS_best['params'])
         SAS_test = SAS_Controller.test(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
         print("SAS candidate achieved validation acccuracy of " + str(SAS_best['accuracy']) + " before further training ")
@@ -106,13 +135,19 @@ def main():
                                             batch_size=32,random_seed=seed,mute=True,
                                             test=True,noVal=False,log_path=SASpath,class_curve=True)
 
-        #revert to pretrained weights if accuracy doesn't improve
+        '''
+        revert to pretraining weights if accuracy doesn't improve
+        '''
         print("SAS candidate achieved test accuracy " + str(SAS_accuracy[1]) + " after further training")
         if(SAS_accuracy[1] <= SAS_test[1]):
             SAS_Controller.param_import(SAS_best['params'])
             print("revert to pretrained weights with test accuracy of " + str(SAS_test[1]) + " before further training")
         SAS_Controller.class_accuracy(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
         
+        '''
+        Retrain a baseline from scratch using the same architecture
+        This baseline is reported in the graph of the paper 
+        '''
         base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,
                                                 output_dim=(i+1),lr=lr,
                                                 epochs=epochs,batch_size=32,
@@ -123,25 +158,14 @@ def main():
         SAS_params = SAS_Controller.export()
         print("storing best result from time step " +str(i))
         print(datetime.datetime.now())
+
+        '''
+        store the best model from this step 
+        '''
         with open(str(i)+'.pkl', 'wb') as f:
             pickle.dump(SAS_Controller, f, pickle.HIGHEST_PROTOCOL)
         print('')
         print('')
-
-    #train with all MNIST data in the end
-    SAS_accuracy = SAS_Controller.execute(datasets[0],input_dim=784,
-                                        output_dim=(i+1),lr=lr,epochs=epochs,
-                                        batch_size=32,random_seed=seed,mute=True,
-                                        test=True,noVal=True,log_path="none",class_curve=True)
-    print("SAS achieved test accuracy " + str(SAS_accuracy[1]) + " with all MNIST data")
-
-    base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,
-                                            output_dim=(i+1),lr=lr,
-                                            epochs=epochs,batch_size=32,
-                                            random_seed=seed,mute=True,
-                                            test=True,noVal=True,upload=False,class_curve=True)
-    print("Baseline achieved test accuracy " + str(base_accuracy[1]) + " with all MNIST data")
-
 
 if __name__ == "__main__":
    main()
--- run_Incremental_SAS.py
+++ run_Incremental_SAS.py
@@ -35,6 +35,12 @@ def main():
                                     random_seed=seed,mute=True,test=True,class_curve=True)
     print("Teacher network achieved test accuracy " + str(accuracy[1]))
     params = main.export()
+
+    '''
+    SAS_Controller manages neural architecture internally
+    details see MLP_Controller/Controller.py
+    SAS stands for simple architecture search (it is the neural architecture search principle used here)
+    '''
     SAS_Controller = Controller.Controller(seed,'sas')
     SAS_Controller.param_import(params)
 
@@ -45,9 +51,16 @@ def main():
         print ("--------------------------------------------------------------")
         print("training step " + str(i-9))
         print ("saturate with new data")
+
+        '''
+        concatenate new data
+        '''
         datasets = mlp_datasets.sum_data(datasets,i,0)
         SASpath = "./logs/"+str(i-9)+"_1SAS"
 
+        '''
+        Train with concatenated dataset using early stopping
+        '''
         SAS_accuracy = SAS_Controller.execute(datasets[0],input_dim=784,
                                             output_dim=(i+1),lr=lr,epochs=epochs,
                                             batch_size=32,random_seed=seed,mute=True,
@@ -56,11 +69,19 @@ def main():
         print ("before SAS search, model achieved test accuracy of " + str(SAS_accuracy[1]))
         SAS_Controller.class_accuracy(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
 
+        '''
+        Retrain a baseline from scratch using the same architecture
+        '''
         base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,epochs=epochs,batch_size=32,random_seed=seed,mute=True,
                                                             test=True,noVal=False,upload=False,class_curve=True)
         print("Baseline achieved test accuracy " + str(base_accuracy[1]))
         SAS_params = SAS_Controller.export()
-        
+
+        '''
+        Simple Architecture Search using 6 predefined guide lines 
+        For details see MLP_archSearch/MLP_SAS.py
+        obtained the best architecture for this step 
+        '''
         SAS_best = MLP_SAS.simple_search(datasets[0],SAS_params,
                                         SAS_accuracy[1],N_workers,
                                         restart_num=restart_num,output_dim=(i+1),
@@ -70,24 +91,33 @@ def main():
         print ("--------------------------------------------------------------")
         print("Doing Further Training")
 
+        '''
+        ensure that the training is complete for the current dataset 
+        usually runs very few epoches
+        '''
         SAS_Controller.param_import(SAS_best['params'])
         SAS_test = SAS_Controller.test(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
         print("SAS candidate achieved validation acccuracy of " + str(SAS_best['accuracy']) + " before further training ")
         print("SAS candidate achieved test accuracy of " + str(SAS_test[1]) + " before further training ")
         SASpath = "./logs/"+str(i)+"_2SAS"
-
         SAS_accuracy = SAS_Controller.execute(datasets[0],input_dim=784,
                                             output_dim=(i+1),lr=lr,epochs=epochs,
                                             batch_size=32,random_seed=seed,mute=True,
                                             test=True,noVal=False,log_path=SASpath,class_curve=True)
 
-        #revert to pretrained weights if accuracy doesn't improve
+        '''
+        revert to pretraining weights if accuracy doesn't improve
+        '''
         print("SAS candidate achieved test accuracy " + str(SAS_accuracy[1]) + " after further training")
         if(SAS_accuracy[1] <= SAS_test[1]):
             SAS_Controller.param_import(SAS_best['params'])
             print("revert to pretrained weights with test accuracy of " + str(SAS_test[1]) + " before further training")
         SAS_Controller.class_accuracy(datasets[0],input_dim=784,output_dim=(i+1),lr=lr,batch_size=32,mute=True)
         
+        '''
+        Retrain a baseline from scratch using the same architecture
+        This baseline is reported in the graph of the paper 
+        '''
         base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,
                                                 output_dim=(i+1),lr=lr,
                                                 epochs=epochs,batch_size=32,
@@ -98,26 +128,14 @@ def main():
         SAS_params = SAS_Controller.export()
         print("storing best result from time step " +str(i))
         print(datetime.datetime.now())
+
+        '''
+        store the best model from this step 
+        '''
         with open(str(i)+'.pkl', 'wb') as f:
             pickle.dump(SAS_Controller, f, pickle.HIGHEST_PROTOCOL)
         print('')
         print('')
-
-
-    #train with all data in the end
-    SAS_accuracy = SAS_Controller.execute(datasets[0],input_dim=784,
-                                        output_dim=(i+1),lr=lr,epochs=epochs,
-                                        batch_size=32,random_seed=seed,mute=True,
-                                        test=True,noVal=True,log_path="none",class_curve=True)
-    print("SAS achieved test accuracy " + str(SAS_accuracy[1]) + " with all MNIST + Fashion-MNIST data")
-
-    base_accuracy = SAS_Controller.baseline(datasets[0],input_dim=784,
-                                            output_dim=(i+1),lr=lr,
-                                            epochs=epochs,batch_size=32,
-                                            random_seed=seed,mute=True,
-                                            test=True,noVal=True,upload=False,class_curve=True)
-    print("Baseline achieved test accuracy " + str(base_accuracy[1]) + " with all MNIST + Fashion-MNIST data")
-
 
 if __name__ == "__main__":
    main()
